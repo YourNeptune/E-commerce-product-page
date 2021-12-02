be68c3ec6e89835158fbaa0b052f6bc9a262d015
LixuanLuo <lixuan@greencubeanalytics.com>
2021-12-01 22:39:12 -0500

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logoSVG from "../images/logo.svg";
+import cartSVG from "../images/icon-cart.svg"
+import avatar from "../images/image-avatar.png"
+
+const NavBar = () => {
+  return (
+    <div>
+      <Navbar>
+        <Container className="container">
+          <Navbar.Brand href="#home">
+            <img src={logoSVG} className="logo"/>
+          </Navbar.Brand>
+          <Nav className="nav-part1 me-auto">
+            <Nav.Link href="#Collection" className="nav-part1--margin">Collection</Nav.Link>
+            <Nav.Link href="#Men" className="nav-part1--margin">Men</Nav.Link>
+            <Nav.Link href="#Women" className="nav-part1--margin">Women</Nav.Link>
+            <Nav.Link href="#About" className="nav-part1--margin">About</Nav.Link>
+            <Nav.Link href="#Contact" className="nav-part1--margin">Contact</Nav.Link>
+          </Nav>
+          <Nav className="nav-part2">
+            <Nav.Link href="#Cart"><img src={cartSVG} /></Nav.Link>
+            <Nav.Link href="#Profile"><img src={avatar} className="avatar"/></Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
